ba28066ef2c5175b08b1d46e8931f957245611b0
skyg547 <skyg547>
2021-01-19 00:25:26 +0900

    운동복

--- datatype/KeyValueType.py
+++ datatype/KeyValueType.py
@@ -18,26 +18,26 @@ print(key_list)
 print(value_list)
 
 # 집합 자료형 = 해시셋
-data = set([1,1,1,2,3,4,5])
+data = set([1, 1, 1, 2, 3, 4, 5])
 print(data)
 
-data = {1,2,3,4,5,5,6,1,6}
+data = {1, 2, 3, 4, 5, 5, 6, 1, 6}
 print(data)
 
-a = set([1,2,3,4,5])
-b = set([3,4,5,6,7])
-#합집합
-print(a|b)
-#교집합
-print(a&b)
-#차집합
-print(a-b)
+a = set([1, 2, 3, 4, 5])
+b = set([3, 4, 5, 6, 7])
+# 합집합
+print(a | b)
+# 교집합
+print(a & b)
+# 차집합
+print(a - b)
 
-data = set([1,2,3])
+data = set([1, 2, 3])
 print(data)
 data.add(4)
 print(data)
-data.update([5,6,7,8,9])
+data.update([5, 6, 7, 8, 9])
 print(data)
 data.remove(3)
 print(data)
