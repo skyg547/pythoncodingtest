ef61613757729ed6e79042bcc2d039276ee23d1e
skyg547 <skyg547@gmail.com>
2021-01-22 23:05:18 +0900

    주식가격

--- QANDS/stockprice.py
+++ QANDS/stockprice.py
@@ -49,3 +49,18 @@ def solution(prices):
 
 a = [5,8,6,2,4,1]
 solution(a)
+
+
+def solution1(prices):
+    answer = [0 for _ in range(len(prices))]
+    stack = []
+    for i in range(len(prices)):
+        while len(stack) != 0 and prices[i] < prices[stack[len(stack) -1]]:
+            temp = stack.pop()
+            answer[temp] = i - temp
+        stack.append(i)
+    while len(stack):
+        temp = stack.pop()
+        answer[temp] = len(prices) - temp - 1
+
+    return answer
