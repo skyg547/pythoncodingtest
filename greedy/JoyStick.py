f7303ba52860ffab7879631fc83dbccbe1da3bfd
skyg547 <skyg547@gmail.com>
2021-02-26 12:35:12 +0900

    조이스틱 정답

--- greedy/JoyStick.py
+++ greedy/JoyStick.py
@@ -29,5 +29,27 @@
 # ※ 공지 - 2019년 2월 28일 테스트케이스가 추가되었습니다.
 
 def solution(name):
+    #상하 조정
+    change = [min(ord(i)-ord('A'), ord('Z')- ord(i)+1 ) for i in name]
+    idx = 0
     answer = 0
-    return answer
+    while True:
+        answer += change[idx]
+        change[idx] = 0
+        if sum(change) == 0:
+            return answer
+
+        # 좌우 이동방향을 정하기
+
+        left, right = 1,1
+        while change[idx - left] == 0:
+            left += 1
+        while change[idx + right] == 0:
+            right += 1
+        # 위치(인덱스) 조정
+        answer += left if left < right else right
+        idx += -left if left < right else right
+
+
+name = "JEROEN"
+print(solution(name))
