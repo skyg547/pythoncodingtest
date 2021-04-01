ee65f2d403c6d168c44a4c4ada55ca8727af76cd
SungHo Ha <skyg547@gmail.com>
2021-04-01 22:53:23 +0900

    암호만들기

--- Search/MakePassword.py
+++ Search/MakePassword.py
@@ -2,12 +2,15 @@ from itertools import permutations
 
 
 def solution(n, s):
+    # 메모리초과 의심부분 1
     lists = list(map("".join, permutations(s, n)))
 
     temp = []
     for i in lists:
+        # 메모리초과 의심부분 2
         if i == ''.join(sorted(i)):
             temp.append(i)
+        lists.remove(i)
     return temp
 
 
@@ -15,7 +18,7 @@ def solution(n, s):
 # ls = ['a', 't', 'c', 'i', 's', 'w']
 # print(ls)
 ls = []
-n, s = map(int,input().split())
+n, s = map(int, input().split())
 # for i in range(int(s)):
 #     ls.append(input())
 ls = list(input().split())
