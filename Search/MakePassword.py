87dc8041f0783852d8c884938e1763db38486e4e
SungHo Ha <skyg547@gmail.com>
2021-04-01 19:36:45 +0900

    암호만들기 -풀이 (메모리초과)

--- Search/MakePassword.py
+++ Search/MakePassword.py
@@ -2,12 +2,24 @@ from itertools import permutations
 
 
 def solution(n, s):
-    print(set(list(sorted(map(''.join, permutations(s, n))))))
+    lists = list(map("".join, permutations(s, n)))
 
-    return 0
+    temp = []
+    for i in lists:
+        if i == ''.join(sorted(i)):
+            temp.append(i)
+    return temp
 
 
-n, s = 4, 6
-ls = ['a', 't', 'c', 'i', 's', 'w']
+# n, s = 4, 6
+# ls = ['a', 't', 'c', 'i', 's', 'w']
+# print(ls)
+ls = []
+n, s = map(int,input().split())
+# for i in range(int(s)):
+#     ls.append(input())
+ls = list(input().split())
 
-print(solution(n, ls))
+# print(ls)
+for i in solution(n, ls):
+    print(i)
