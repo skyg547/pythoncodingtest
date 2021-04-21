23541139ff4831d71df32de1f4a12838f979ee5d
skyg547 <skyg547@gmail.com>
2021-04-21 15:24:45 +0900

    BOJ7562KnightMoving.py solved

--- BFS/BOJ7562KnightMoving.py
+++ BFS/BOJ7562KnightMoving.py
@@ -19,8 +19,6 @@
 # # 깊이가 너무 깊어 bfs 로 풀어야 한다 .
 # import sys
 #
-# dx = [-2, -1, 1, 2, -2, -1, 1, 2]
-# dy = [-1, -2, -2, -1, 1, 2, 2, 1]
 #
 #
 #
@@ -49,41 +47,69 @@
 #
 from collections import deque
 
-def solution(l,x,y):
+dx = [-2, -1, 1, 2, -2, -1, 1, 2]
+dy = [-1, -2, -2, -1, 1, 2, 2, 1]
 
+
+def solution(l, x, y):
+    # print(x, y)
     # 일단 bfs 로 돌면서 각 최소값들을 체크해서 판을 모두 채우자
-
     # 그러면 방문한 곳은 0 이 아닌 만들어 주자
 
     # 일단 판을 만들고
-    gamMap = [[-1]*l for i in range(l)]
+    gamMap = [[-1] * l for i in range(l)]
 
     # for index in gamMap:
     #     for element in range(l):
     #         index.append(-1)
 
     # 게임 맵이 -1 이 아니면
-    for y in range(l):
-        for x in
+    # bfs 수행
+    queue = deque()
+    # 시작점 넣기
+    queue.append(x)
+    # 시작 점 방문 처리
+    gamMap[x[0]][x[1]] = 0
+    # print(queue)
+    # 이동 횟수
+    cnt = 0
+    move = 0
+    tempmove = 0
+    # bfs
+    while queue[0] != y:
+        # 큐에서 지금 위치 꺼내기
+        location = queue.popleft()
 
-        queue if
-            queue = deque()
+        for index in range(8):
+            # 게임판 을 벗어나는지 체크하기
+            if 0 <= location[0] + dy[index] < l and 0 <= location[1] + dx[index] < l:
+                # 만약에 방문이 안된곳이라면
+                if gamMap[location[0] + dy[index]][location[1] + dx[index]] == -1:
+                    # 큐에 없고
+                    # --> not in 연산 시간이 오래 걸려서 시간 초과
+                    # if [location[0] + dy[index], location[1] + dx[index]] not in queue:
+                        # 이동 횟수로 바꿔주고 -> 이동 횟수가 아닌 바로 전꺼를 더해주자!
+                        gamMap[location[0] + dy[index]][location[1] + dx[index]] = gamMap[location[0]][location[1]] + 1
+                        # 큐에 추가 해줍니다.
+                        queue.append([location[0] + dy[index], location[1] + dx[index]])
+        # print(queue)
 
-        # bfs 수행
-
-
-    return
-
+    # print(*gamMap)
+    print(gamMap[y[0]][y[1]])
 
 
 if __name__ == '__main__':
 
-    n = 3
+    n = int(input())
+    array = []
 
-    l = 8
-    x = [0, 0]
+    for _ in range(n):
+        l = int(input())
 
-    y = [7, 0]
+        x = list(map(int, input().split()))
+        y = list(map(int, input().split()))
+        array.append([l, x, y])
 
-    print(solution(l, x, y))
-
+        pass
+    for i in array:
+        solution(i[0], i[1], i[2])
