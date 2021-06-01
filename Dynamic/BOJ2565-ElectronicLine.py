b822e8d1daa50f3774b47cf9a1e76d3063a1a027
SungHo Ha <skyg547@gmail.com>
2021-06-01 20:53:14 +0900

    test commit

--- Dynamic/BOJ2565-ElectronicLine.py
+++ Dynamic/BOJ2565-ElectronicLine.py
@@ -13,26 +13,53 @@ def solution(l):
     l.sort()
     print(l)
 
-    subAns = []
+    # 실패 오답
+    # subAns = []
+    # #
+    # # 차를 저장
+    # for element in l:
+    #     subAns.append(abs(element[1] - element[0]))
+    # 실패한 오답
+    # # 첫번째 숫자가 클 때
+    # for index in range(1, len(l)):
+    #     if l[index][1] < l[index - 1][1]:
+    #     #     # 둘의 차가 크다면 지워주기
+    #
+    #         if subAns[index] < subAns[index - 1]:
+    #             ans += 1
+    # l.sort(reverse=True)
+    #
+    # for index in range(1, len(l)):
+    #     if l[index][1] < l[index][1]:
+    #         if subAns
+    #
 
-    # 차를 저장
-    for element in l:
-        subAns.append(abs(element[1] - element[0]))
+    #LIS로 풀이
 
-    # 첫번째 숫자가 클 때
-    for index in range(1, len(l)):
-        if l[index][1] < l[index - 1][1]:
-        #     # 둘의 차가 크다면 지워주기
+    # LIS를 구해주자
+    result = [[] for _ in range(len(l))] # LIS 담을 배렬
 
-            if subAns[index] < subAns[index - 1]:
-                ans += 1
-    l.sort(reverse=True)
+    # 각 원소 마다 돌면서
+    for i in range(len(l)):
+        # 맨 처음은 하나
+        if i == 0:
+            result[i].append(l[i][1])
+        # 아니면
+        else:
+            # i 까지 돌아보면서
+            for j in range(0, i):
+                # 만약 최장거리 배열이 최근 값보다 크고
+                if result[j][-1] < l[i][1]:
+                    #
+                    if len(result[i]) - 1 < len(result[j]):
+                        result[i] = result[j] + [l[i][1]]
+            # 아무것도 없다면
+            if not result[i]:
+                result[i].append(l[i][1])
+    for i in range(len(l)):
+        ans = max(ans, len(result[i]))
 
-    for index in range(1, len(l)):
-        if l[index][1] < l[index][1]:
-            if subAns
-
-    return ans
+    return len(l)-ans
 
 
 if __name__ == '__main__':
@@ -45,4 +72,7 @@ if __name__ == '__main__':
          [10, 10],
          [9, 7],
          [7, 6]]
+
+    # s = int(input())
+    # l = [list(map(input().split())) for _ in range(s)]
     print(solution(l))
