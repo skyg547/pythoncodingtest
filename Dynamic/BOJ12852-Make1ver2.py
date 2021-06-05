506c36d6cd197f205a0018fa24a18666860e1c96
SungHo Ha <skyg547@gmail.com>
2021-06-06 04:31:46 +0900

    BOJ12852-Make1ver2.py fail3

--- Dynamic/BOJ12852-Make1ver2.py
+++ Dynamic/BOJ12852-Make1ver2.py
@@ -1,10 +1,10 @@
 # https://www.acmicpc.net/problem/12852
 # 1로 만들기 버전 2
-
+import copy
 import sys
 
 
-#dp로 만들어서 해결하기
+# dp로 만들어서 해결하기
 
 
 #
@@ -28,32 +28,46 @@ import sys
 #         return answer1
 
 def solution(n):
-    ans = [0]*(10**6+1)
-
+    answer = [100000000] * (10 ** 6 + 1) # 그냥배열
+    answer2 = [[1]] * (10 ** 6 + 1) # 경로배열
+    answer3 = []
     # 초기 3갯값 설정
-    ans[1] , ans[2], ans[3] = 1, 1, 1
-
+    answer[1], answer[2], answer[3] = 1, 1, 1
+    answer2[2], answer2[3] = [1, 2], [1, 3]
     # 1,2,3이면 1 반환
     if 0 < n < 4:
-        return 1
+        return answer[n], answer2[n]
 
-    # 4 부터 돌기 시작
-    for index in range(4, n+1):
-        # ans의 값은 3개 중 가장 작은거
-        ans[index] = min(ans[index-1], ans[index//3], ans[index//2])+1
-
-        # 각 연산을 한번씩 해봐야함 ?
-
-        # 1
-    return ans[n]
-    print(ans)
-
-
-
+    # 1부터 돌기
+    for index in range(1, n):
+        # 지금 꺼에 1을 더한게 더 작다면 ?
+        if answer[index] + 1 < answer[index + 1]:
+            answer[index + 1] = answer[index] + 1 # 작은값으로 대체
+            answer2[index + 1] = copy.deepcopy(answer2[index]) # 경로도 대체
+            answer2[index + 1].append(index + 1) # 그 값 더해주기
+            answer3.append(index)
+        # 만약 3을 곱했을 때 n을 넘기지 않는다면
+        if index <= (n // 3):
+            if answer[index] + 1 < answer[index * 3]:
+                answer[index * 3] = answer[index] + 1
+                answer2[index * 3] = copy.deepcopy(answer2[index])
+                answer2[index * 3].append(index * 3)
+                answer3.append(index)
+        # 만약 2를 곱했을 때 n을 넘기지 않는다면
+        if index <= (n // 2):
+            if answer[index] + 1 < answer[(index * 2)]:
+                answer[index * 2] = answer[index] + 1
+                answer2[index * 2] = copy.deepcopy(answer2[index])
+                answer2[index * 2].append(index * 2)
+                answer3.append(index)
+    print(answer3)
+    return answer[n], answer2[n]
 
 
 if __name__ == '__main__':
-    print(solution(10))
+    answer = (solution(int(sys.stdin.readline())))
 
+    print(answer[0])
+    print(*answer[1][::-1])
 # 3
 # 10  9 3 1
