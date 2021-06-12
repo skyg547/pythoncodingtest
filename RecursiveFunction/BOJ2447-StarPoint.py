af9cf3112d6bef7f8cbc4d89ca0e53cc78b09278
skyg547 <skyg547@gmail.com>
2021-06-12 22:58:58 +0900

    BOJ2447-StarPoint.py fail

---
+++ RecursiveFunction/BOJ2447-StarPoint.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/2447
+# 별찍기
+
+# # 배열
+# arrays = [['*']*3 for _ in range(3)]
+#
+#
+# def solution(n):
+#     if n == len(arrays):
+#         return
+#     if n % 3 == 1:
+#         arrays[n][n] = ' '
+#
+#     solution(n+1)
+#
+#
+# if __name__ == '__main__':
+#     solution(0)
+#
+#
+#
+
+def main():
+    ans = []
+    n = int(input())
+
+    for dy in range(n):
+        for
