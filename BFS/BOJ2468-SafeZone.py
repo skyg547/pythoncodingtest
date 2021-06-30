b6cbb2288ae4e90f572fed4c227e4caedb38de98
skyg547 <skyg547@gmail.com>
2021-06-30 22:52:10 +0900

    BOJ2468-SafeZone.py problem

---
+++ BFS/BOJ2468-SafeZone.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2468
+# 안전 영역
+
+def solution(l):
+    return
+
+
+if __name__ == '__main__':
+    s = 5
+    l = [[6, 8, 2, 6, 2],
+         [3, 2, 3, 4, 6],
+         [6, 7, 3, 3, 2],
+         [7, 2, 5, 3, 6],
+         [8, 9, 5, 2, 7]]
+    print(solution(l))
