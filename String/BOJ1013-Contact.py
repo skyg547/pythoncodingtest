648f0a123eab7d91dd1e00848f993f6cd6f53466
skyg547 <skyg547@gmail.com>
2021-07-01 20:40:54 +0900

    BOJ1013-Contact.py problem

---
+++ String/BOJ1013-Contact.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1013
+# Contact
+
+def solution(l):
+    return
+
+
+if __name__ == '__main__':
+    s = 3
+    l = ["10010111",
+         "011000100110001",
+         "0110001011001"]
+    print(solution(l))
