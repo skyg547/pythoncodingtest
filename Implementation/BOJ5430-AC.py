2a66cd206efcc8d9c405d99d981cbf6ed68b991c
skyg547 <skyg547@gmail.com>
2021-07-02 21:43:13 +0900

    BOJ5430-AC.py problem

---
+++ Implementation/BOJ5430-AC.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/5430
+# AC
+
+def solution():
+    return
+
+
+if __name__ == '__main__':
+    l = [[4],
+         ["RDD"],
+         [4],
+         [[1, 2, 3, 4]]
+         ["DD"],
+         [1],
+         [[42]],
+         ["RRD"],
+         [6],
+         [[1, 1, 2, 3, 5, 8]],
+         ["D"],
+         [0],
+         [[]]]
