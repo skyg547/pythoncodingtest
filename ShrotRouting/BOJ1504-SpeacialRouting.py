5e22384339ec92644753fd4b3d695797845b8583
skyg547 <skyg547@gmail.com>
2021-07-03 17:48:32 +0900

    BOJ1504-SpeacialRouting.py problem

---
+++ ShrotRouting/BOJ1504-SpeacialRouting.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1504
+# 특정한 최단 경로
+
+def solution():
+    return
+
+
+if __name__ == '__main__':
+    n, e = 4, 6
+    l = [[1, 2, 3],
+         [2, 3, 3],
+         [3, 4, 1],
+         [1, 3, 5],
+         [2, 4, 5],
+         [1, 4, 4]]
+    v1, v2 = 2, 3
