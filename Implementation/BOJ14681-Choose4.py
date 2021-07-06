89d89c51a03a03a7d7cd4acd47231b1d2f6fd05c
skyg547 <skyg547@gmail.com>
2021-07-06 23:52:21 +0900

    BOJ14681-Choose4.py solving

---
+++ Implementation/BOJ14681-Choose4.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/14681
+# 사분면 고르기
+
+import sys
+
+def solution(x,y):
+
+    mul = x*y
+    add = x+y
+
+    if mul < 0
+    return
+
+
+if __name__ == '__main__':
+
+    x = int(input())
+    y = int(input())
+
+    solution(x,y)
