5c7337810326724980eac4717813d8b34d825d3f
skyg547 <skyg547@gmail.com>
2021-07-08 22:00:14 +0900

    BOJ14681-Choose4.py solve

--- Implementation/BOJ14681-Choose4.py
+++ Implementation/BOJ14681-Choose4.py
@@ -3,18 +3,31 @@
 
 import sys
 
-def solution(x,y):
 
-    mul = x*y
-    add = x+y
+def solution(x, y):
+    mul = x * y
+    add = x + y
 
-    if mul < 0
-    return
+    if mul < 0 and add <= 0:
+        if x < 0:
+            return 2
+        else:
+            return 4
+
+    elif mul < 0 <= add:
+        if x > 0:
+            return 4
+        else:
+            return 2
+    elif mul > 0 > add:
+        return 3
+
+    else:
+        return 1
 
 
 if __name__ == '__main__':
-
     x = int(input())
     y = int(input())
 
-    solution(x,y)
+    print(solution(x, y))
