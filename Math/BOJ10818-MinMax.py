99755f6aeca8bbaec53ee8c1febf5349423938cf
skyg547 <skyg547@gmail.com>
2021-07-12 22:47:32 +0900

    BOJ10818-MinMax.py solve

---
+++ Math/BOJ10818-MinMax.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/10818
+# 최소 최대
+def solution():
+    return
+
+
+if __name__ == '__main__':
+    l = int(input())
+    s = list(map(int, input().split()))
+
+    print(min(s), max(s))
