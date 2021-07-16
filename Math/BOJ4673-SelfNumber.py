bd62fa8298752d55f4750818921d83b1af89fb1a
skyg547 <skyg547@gmail.com>
2021-07-16 23:48:20 +0900

    BOJ4673-SelfNumber.py fail

--- Math/BOJ4673-SelfNumber.py
+++ Math/BOJ4673-SelfNumber.py
@@ -1,9 +1,31 @@
-# https://www.acmicpc.net/problem/4673
-# 셀프 넘버
-
+# # https://www.acmicpc.net/problem/4673
+# # 셀프 넘버
+#
 
 def solution():
-
     return
 
+
 if __name__ == '__main__':
+    x = 1
+
+    result = []
+    for index in range(10001):
+        result.append(index)
+
+    for x in range(10001):
+        y = x
+        #print("초기 x =", x)
+        y += (x % 10) # 1의 자리수 제거
+        y += ((x % 100) // 10) # 10의 자리수
+        y += ((x % 1000)// 100) #100의 자리수
+        y += ((x % 10000) // 1000) #1000의 자리수
+
+        if y > 10000:
+            break
+        if y in result:
+            result.remove(int(y))
+
+    for element in result:
+        print(element)
+
