10f4433c48a2572e1dbe918cb29f878c3edad4b0
SungHo Ha <skyg547@gmail.com>
2021-07-17 22:33:07 +0900

    BOJ4673-SelfNumber.py solve

--- Math/BOJ4673-SelfNumber.py
+++ Math/BOJ4673-SelfNumber.py
@@ -7,19 +7,22 @@ def solution():
 
 
 if __name__ == '__main__':
-    x = 1
 
     result = []
-    for index in range(10001):
+    for index in range(9994):
         result.append(index)
 
     for x in range(10001):
+
         y = x
-        #print("초기 x =", x)
-        y += (x % 10) # 1의 자리수 제거
-        y += ((x % 100) // 10) # 10의 자리수
-        y += ((x % 1000)// 100) #100의 자리수
-        y += ((x % 10000) // 1000) #1000의 자리수
+        # # print("초기 x =", x)
+        # y += (x % 10)  # 1의 자리수 제거
+        # y += ((x % 100) // 10)  # 10의 자리수
+        # y += ((x % 1000) // 100)  # 100의 자리수
+        # y += ((x % 10000) // 1000)  # 1000의 자리수
+        stringX = str(x)
+        for element in stringX:
+            y += int(element)
 
         if y > 10000:
             break
@@ -28,4 +31,3 @@ if __name__ == '__main__':
 
     for element in result:
         print(element)
-
