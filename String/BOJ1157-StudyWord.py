c21d9511bc86301c71b9aea26d719d3cef293d75
skyg547 <skyg547@gmail.com>
2021-07-28 22:27:31 +0900

    BOJ2675-StringRepeat.py solve

--- String/BOJ1157-StudyWord.py
+++ String/BOJ1157-StudyWord.py
@@ -1,18 +1,20 @@
 # https://www.acmicpc.net/problem/1157
 # 단어공부
-from collections import  Counter
+from collections import Counter
+
 
 def solution(strings):
     ansArray = Counter(strings).most_common()
     if ansArray[0][1] is ansArray[1][1]:
         return '?'
-    return  str(ansArray[0][0]).upper()
+    return str(ansArray[0][0]).upper()
+
 
 if __name__ == '__main__':
 
-    strings = input()
+    strings = input().lower()
 
-    if(len(strings) == 1 ):
+    if (len(strings) == 1):
         print(strings.upper())
         exit()
     countString = dict()
@@ -24,9 +26,21 @@ if __name__ == '__main__':
         else:
             countString[chars] = 1
 
-    maxValue = max(countString)
+    maxValue = max(countString.values())
+    count = 0
 
+    for countElement in countString.values():
 
-    print(solution(strings.lower()))
+        if countElement == maxValue:
+            count += 1
 
+        if count == 2:
+            print("?")
+            exit()
 
+    for element in countString.items():
+        if maxValue ==element[1]:
+            print(element[0].upper())
+            break
+
+    # print(solution(strings.lower()))
