ac5d9d31de1e01892fcd5f7cfb492258da46dfbe
skyg547 <skyg547@gmail.com>
2021-08-13 07:22:52 +0900

    BOJ1316-GroupWordCheeky.py solving

--- String/BOJ1316-GroupWordCheeky.py
+++ String/BOJ1316-GroupWordCheeky.py
@@ -2,8 +2,23 @@
 # 그룹단어체키
 
 def solution(word):
+    answer = 0
+    # for element in word:
+    #     if element i
 
-    return
+    answerSet = set()
+    temp = ""
+    word = "abab"
+
+    for element in word:
+        answerSet.add(element)
+        answerSet.add(temp)
+
+
+    # 나온적 있으면 set 추가
+    # 모든 나온걸 set 에 추가 하고 검사하자
+    # 들어있느지 안들어 있는지
+    return answer
 
 if __name__ == '__main__':
 
