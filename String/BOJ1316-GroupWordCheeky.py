54d20fb2ac1b154b4d0e2ef22d172a79dc8b569d
skyg547 <skyg547@gmail.com>
2021-08-12 23:33:16 +0900

    BOJ1316-GroupWordCheeky.py solving

--- String/BOJ1316-GroupWordCheeky.py
+++ String/BOJ1316-GroupWordCheeky.py
@@ -1,12 +1,16 @@
 # https://www.acmicpc.net/problem/1316
 # 그룹단어체키
 
-def solution():
+def solution(word):
 
     return
 
 if __name__ == '__main__':
-    3
-    happy
-    new
-    year
+
+    loop = int(input())
+    count = 0
+
+    for _ in range(loop):
+        count += solution(input())
+
+    print(count)
