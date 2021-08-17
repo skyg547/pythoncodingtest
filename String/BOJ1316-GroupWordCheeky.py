50df8511854ac50284c65ef81940033c55945125
skyg547 <skyg547@gmail.com>
2021-08-17 22:48:33 +0900

    BOJ1316-GroupWordCheeky.py answer

--- String/BOJ1316-GroupWordCheeky.py
+++ String/BOJ1316-GroupWordCheeky.py
@@ -13,8 +13,7 @@ def solution(word):
     for element in word:
         answerSet.add(element)
         answerSet.add(temp)
-
-
+    # 1.  dusthr
     # 나온적 있으면 set 추가
     # 모든 나온걸 set 에 추가 하고 검사하자
     # 들어있느지 안들어 있는지
@@ -29,3 +28,22 @@ if __name__ == '__main__':
         count += solution(input())
 
     print(count)
+
+
+
+
+# 정답
+    n = int(input())
+
+    group_word = 0
+    for _ in range(n):
+        word = input()
+        error = 0
+        for index in range(len(word) - 1):  # 인덱스 범위 생성 : 0부터 단어개수 -1까지
+            if word[index] != word[index + 1]:  # 연달은 두 문자가 다른 때,
+                new_word = word[index + 1:]  # 현재글자 이후 문자열을 새로운 단어로 생성
+                if new_word.count(word[index]) > 0:  # 남은 문자열에서 현재글자가 있있다면
+                    error += 1  # error에 1씩 증가.
+        if error == 0:
+            group_word += 1  # error가 0이면 그룹단어
+    print(group_word)
