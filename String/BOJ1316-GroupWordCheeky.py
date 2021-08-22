31da8b1e43cef5aeb08cb3187b682771d8563ecf
skyg547 <skyg547@gmail.com>
2021-08-22 23:14:45 +0900

    Group word cheeky debuging

--- String/BOJ1316-GroupWordCheeky.py
+++ String/BOJ1316-GroupWordCheeky.py
@@ -1,33 +1,33 @@
 # https://www.acmicpc.net/problem/1316
 # 그룹단어체키
-
-def solution(word):
-    answer = 0
-    # for element in word:
-    #     if element i
-
-    answerSet = set()
-    temp = ""
-    word = "abab"
-
-    for element in word:
-        answerSet.add(element)
-        answerSet.add(temp)
-    # 1.  dusthr
-    # 나온적 있으면 set 추가
-    # 모든 나온걸 set 에 추가 하고 검사하자
-    # 들어있느지 안들어 있는지
-    return answer
-
-if __name__ == '__main__':
-
-    loop = int(input())
-    count = 0
-
-    for _ in range(loop):
-        count += solution(input())
-
-    print(count)
+#
+# def solution(word):
+#     answer = 0
+#     # for element in word:
+#     #     if element i
+#
+#     answerSet = set()
+#     temp = ""
+#     word = "abab"
+#
+#     for element in word:
+#         answerSet.add(element)
+#         answerSet.add(temp)
+#     # 1.  dusthr
+#     # 나온적 있으면 set 추가
+#     # 모든 나온걸 set 에 추가 하고 검사하자
+#     # 들어있느지 안들어 있는지
+#     return answer
+#
+# if __name__ == '__main__':
+#
+#     loop = int(input())
+#     count = 0
+#
+#     for _ in range(loop):
+#         count += solution(input())
+#
+#     print(count)
 
 # 정답
 n = int(input())
