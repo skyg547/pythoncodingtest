a4c5824ec3f248a1374e7b9270a0455a11e190fc
skyg547 <skyg547@gmail.com>
2021-08-23 22:15:00 +0900

    BOJ1712-BEP.py Time Out

--- Math/BOJ1712-BEP.py
+++ Math/BOJ1712-BEP.py
@@ -1,5 +1,25 @@
 # https://www.acmicpc.net/problem/1712
+# 손익 분기점
 
 
 def solution():
     return
+
+
+if __name__ == '__main__':
+    a, b, c = map(int, input().split())
+    if c-b <= 0 :
+        print(-1)
+        exit()
+
+    count = 1
+
+    while (c-b) * count <= a :
+        count += 1
+    # count = 1
+    # while a + (b * count) >= c * count:
+    #     count += 1
+    #     print(count)
+
+    print(count)
+
