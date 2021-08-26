38b2e54acf23e645032d7bb3e6845630b5f9388d
SungHo Ha <skyg547@gmail.com>
2021-08-26 22:40:29 +0900

    BOJ1193-Fraction.py problem

---
+++ Math/BOJ1193-Fraction.py
@@ -0,0 +1,9 @@
+# 분수 찾기
+# https://www.acmicpc.net/problem/1193
+
+
+
+
+def solution():
+    return
+
--- Math/BOJ2292-BeeHouse.py
+++
@@ -1,7 +0,0 @@
-#  벌집
-# https://www.acmicpc.net/problem/2292
-
-
-def solution():
-    return
-
