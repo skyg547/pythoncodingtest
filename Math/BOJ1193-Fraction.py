8cefd3d475d2a46b6ee4ef326a29cde63e69dadf
SungHo Ha <skyg547@gmail.com>
2021-08-29 13:31:27 +0900

    BOJ1193-Fraction.py solving with answer

--- Math/BOJ1193-Fraction.py
+++ Math/BOJ1193-Fraction.py
@@ -1,11 +1,28 @@
 # 분수 찾기
 # https://www.acmicpc.net/problem/1193
 
-
-
 def solution():
-    a, b ,c   = map(int, input().split())
+    a, b, c = map(int, input().split())
 
+    x = int(input())
+    num_list = []
+
+    num = 0
+    num_count = 0
+
+    while num_count < x:
+        num += 1
+        num_count += num
+
+    num_count -= num
+
+    if num % 2 == 0:
+        i = x - num_count
+        j = num - i + 1
+    else:
+        i = num - (x - num_count) + 1
+        j = x - num_count
+
+    print(f"{i}/{j}")
 
     return
-
