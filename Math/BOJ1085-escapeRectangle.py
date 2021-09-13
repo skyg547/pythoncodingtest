c78083d7971ee1f4bcae755624c4b1172c97486c
skyg547 <skyg547@gmail.com>
2021-09-13 22:06:29 +0900

    BOJ1085-escapeRectangle.py solve

--- Math/BOJ1085-escapeRectangle.py
+++ Math/BOJ1085-escapeRectangle.py
@@ -1,12 +1,13 @@
 # https://www.acmicpc.net/problem/1085
 # 직사각형에서 탈출
-
+# 0.0 랑 가까운지도 생각 하자
 
 def solution():
     return
 
 
 if __name__ == '__main__':
-    x, y, w, h = map(int,input().split())
+    x, y, w, h = map(int, input().split())
 
-    print(min(w-x, h-y))
+    print(min(w - x, h - y,
+              x - 0, y - 0))
