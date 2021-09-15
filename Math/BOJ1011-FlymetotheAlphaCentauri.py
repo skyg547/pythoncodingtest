fee168914aaf33771d06e7b700233be749275ac4
SungHo Ha <skyg547@gmail.com>
2021-09-15 20:34:42 +0900

    BOJ1011-FlymetotheAlphaCentauri.py promblem

---
+++ Math/BOJ1011-FlymetotheAlphaCentauri.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1011
+# Fly me to the Alpha Centauri
+
+def solution():
+    return
+
+
+if __name__ == '__main__':
+    t = 3
+    l = [[0, 3],
+         [1, 5]
+         [45, 50]]
