c19e7e9c67e5b16ef8828a0249542742accd6093
SungHo Ha <skyg547@gmail.com>
2021-09-17 21:17:40 +0900

    BOJ2447-StarPoint.py resolve

--- RecursiveFunction/BOJ2447-StarPoint.py
+++ RecursiveFunction/BOJ2447-StarPoint.py
@@ -1,6 +1,9 @@
 # https://www.acmicpc.net/problem/2447
 # 별찍기
 
+
+# ------------------------------------------------
+
 # # 배열
 # arrays = [['*']*3 for _ in range(3)]
 #
@@ -20,28 +23,44 @@
 #
 #
 
-n = int(input())
+#
+# n = int(input())
+#
+# data = [['*'] * n for _ in range(n)]
+#
+#
+# def remove_stars(length, x, y):
+#     global data
+#     if length == 1:
+#         return
+#
+#     # 지워 주는 부분
+#     for row in data[x - length + length // 3: x - length // 3]:
+#
+#         row[y - length + length // 3:y - length // 3] = ' ' * (length // 3)
+#
+#
+#     #  3 으로 나눠서 들어가는 부툰
+#     for i in range(length // (length // 3)):
+#         for j in range(length // (length // 3)):
+#             remove_stars(length // 3, x - i * length // 3, y - j * length // 3)
+#
+#
+# remove_stars(n, n, n)
+# for row in data:
+#     print(*row)
+#
+#-----------------------------------------------
 
-data = [['*'] * n for _ in range(n)]
+
+def solution():
+    return
 
 
-def remove_stars(length, x, y):
-    global data
-    if length == 1:
-        return
-
-    # 지워 주는 부분
-    for row in data[x - length + length // 3: x - length // 3]:
-
-        row[y - length + length // 3:y - length // 3] = ' ' * (length // 3)
 
 
-    #  3 으로 나눠서 들어가는 부툰
-    for i in range(length // (length // 3)):
-        for j in range(length // (length // 3)):
-            remove_stars(length // 3, x - i * length // 3, y - j * length // 3)
 
 
-remove_stars(n, n, n)
-for row in data:
-    print(*row)
+
+
+
