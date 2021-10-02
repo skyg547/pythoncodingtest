566ed33d65a1b8db9c5e86dba59a4190a9c3f6f3
SungHo Ha <skyg547@gmail.com>
2021-10-02 18:07:19 +0900

    BOJ10989-SortNumber.py problem

---
+++ Sort/BOJ10989-SortNumber.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/10989
+# 수정렬하기 3
+
+def solution():
+    return
+
+
+if __name__ == '__main__':
+    N = 10
+    number = [
+        5,
+        2,
+        3,
+        1,
+        4,
+        2,
+        3,
+        5,
+        1,
+        7]
+    solution()
