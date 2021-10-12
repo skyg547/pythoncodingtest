a7721670d7f132d7a2fe4e23e7516f48631be52b
SungHo Ha <skyg547@gmail.com>
2021-10-12 23:37:59 +0900

    BOJ10989-SortNumber.py solve

--- Sort/BOJ10989-SortNumber.py
+++ Sort/BOJ10989-SortNumber.py
@@ -2,15 +2,22 @@
 # 수정렬하기 3
 
 import sys
+
 input = sys.stdin.readline
+
 
 def solution():
     return
 
 
 if __name__ == '__main__':
+    numberList = [0] * 10001
 
     N = int(input())
-    number = [int(input()) for _ in range(N)]
 
-    print((number).sort())
+    for _ in range(N):
+        numberList[int(input())] += 1
+
+    for number, element in enumerate(numberList):
+        for _ in range(element):
+            print(number)
