8cc1d4ad26e48f8d190a662613c07d0a28584b93
skyg547 <skyg547@gmail.com>
2021-11-25 23:33:12 +0900

    BOJ11605-SortLocation.py solve

--- Sort/BOJ11605-SortLocation.py
+++ Sort/BOJ11605-SortLocation.py
@@ -1,10 +1,13 @@
 def solution(locationArray):
   # locationArray.sort(lambda x : (x[0], x[1]))
 
-  return sorted(locationArray, key =  lambda x : (x[0], -x[1]))
+  return sorted(locationArray, key =  lambda x : (x[0], x[1]))
 
 number = int(input())
 
 locations = [list(map(int, input().split())) for _ in range(number)]
 
-print(solution(locations))
+
+
+for element in solution(locations):
+  print(*element)
