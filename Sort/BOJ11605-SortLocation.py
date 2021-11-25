d05a6cefa73aeb78ce95a8d4982f5719a63d2583
skyg547 <skyg547@gmail.com>
2021-11-25 23:39:21 +0900

    BOJ11651-SortLocation.py solve2

--- Sort/BOJ11605-SortLocation.py
+++ Sort/BOJ11605-SortLocation.py
@@ -8,6 +8,12 @@ number = int(input())
 locations = [list(map(int, input().split())) for _ in range(number)]
 
 
+def solution2(locationArray):
+  # locationArray.sort(lambda x : (x[0], x[1]))
 
-for element in solution(locations):
+  return sorted(locationArray, key =  lambda x : (x[1], x[0]))
+
+
+
+for element in solution2(locations):
   print(*element)
