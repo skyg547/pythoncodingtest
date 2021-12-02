8bbccd61ea22ab9f669de64aefa141e612a00098
SungHo Ha <skyg547@gmail.com>
2021-12-02 23:28:01 +0900

    BOJ1181-Sortword.py first try

--- Sort/BOJ1181-Sortword.py
+++ Sort/BOJ1181-Sortword.py
@@ -1,19 +1,26 @@
 # https://www.acmicpc.net/problem/1181
 
 def solution(x):
-    return sorted(x, key= lambda x : (len(x), x))
+    return sorted(x, key=lambda x: (len(x), x))
 
 
-print(solution(['but',
-                'i',
-                'wont',
-                'hesitate',
-                'no',
-                'more',
-                'no',
-                'more',
-                'it',
-                'cannot',
-                'wait',
-                'im',
-                'yours']))
+number = int(input())
+wordList = [input() for _ in range(number)]
+
+for word in solution(wordList):
+    print(word)
+# print(*solution(wordList))
+#
+# print(solution(['but',
+#                 'i',
+#                 'wont',
+#                 'hesitate',
+#                 'no',
+#                 'more',
+#                 'no',
+#                 'more',
+#                 'it',
+#                 'cannot',
+#                 'wait',
+#                 'im',
+#                 'yours']))
