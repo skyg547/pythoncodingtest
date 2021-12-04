e99bdff42cbb540a6fbdcb7f453cb5c4264fdd89
SungHo Ha <skyg547@gmail.com>
2021-12-04 19:19:27 +0900

    BOJ10814-SortByAge.py first Try

--- Sort/BOJ10814-SortByAge.py
+++ Sort/BOJ10814-SortByAge.py
@@ -1,9 +1,15 @@
 #나이순 정렬 
+# https://www.acmicpc.net/problem/10814
 
 def solution(array):
 
 
-	return sorted(array, key = lambda x : (x[0], x[2]) )
+	return sorted(array, key = lambda x : (x[0], x[1]) )
 
+number = int(input())
 
-print(solution([[21, "junky", 0], [22, "dogu", 1], [21, "ho", 2]]))
+humanList = [list(input().split()) for _ in range(number)]
+
+for listElement in solution(humanList):
+	print(*listElement)
+
