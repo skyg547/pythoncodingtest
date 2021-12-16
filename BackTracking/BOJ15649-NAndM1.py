534165a59658bf2c5cfbcb50e5de4b48413e9864
SungHo Ha <skyg547@gmail.com>
2021-12-16 22:48:31 +0900

    BOJ15649-NAndM1.py 2 try

--- BackTracking/BOJ15649-NAndM1.py
+++ BackTracking/BOJ15649-NAndM1.py
@@ -1,6 +1,7 @@
 def solution(n,m):
     answer = []
-
+    sets = set()
+    sets.add()
     for number in range(1,n+1):
         for number2 in range(1,m+1):
             answer.append(number)
@@ -8,4 +9,4 @@ def solution(n,m):
     return answer
 
 if __name__ == '__main__':
-    print(solution(4,2))
+    print(solution(4,4))
