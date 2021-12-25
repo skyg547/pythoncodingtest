5c6916c1ed5d344c40e6d142b9d17f1d528ca0ab
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2021-12-25 07:07:12 +0000

    solving hanoi

--- RecursiveFunction/BOJ17729-orderHanoiTop.py
+++ RecursiveFunction/BOJ17729-orderHanoiTop.py
@@ -1,10 +1,10 @@
 # 하노이 탑 순서 https://www.acmicpc.net/problem/11729
 
-def solution():
+def solution(n,slip,start):
     return
 
 
 if __name__ == '__main__':
- n = input(prompt); 
+    n = input(); 
 
-    solution()
+    print(solution(n,slip,start))
