30b844d5e2f5e6fad5533d200cc8bd95bbcd1b6b
SungHo Ha <skyg547@gmail.com>
2021-12-28 22:47:29 +0900

    change input fix

--- BackTracking/BOJ15649-NAndM1.py
+++ BackTracking/BOJ15649-NAndM1.py
@@ -1,13 +1,13 @@
-def solution(n,m):
+def solution(n, m):
     answer = []
     sets = set()
-    sets.add()
-    for number in range(1,n+1):
-        for number2 in range(1,m+1):
+    for number in range(1, n + 1):
+        for number2 in range(1, m + 1):
             answer.append(number)
 
     return answer
 
+
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    print(solution(4,4))
+    print(solution(n, m))
