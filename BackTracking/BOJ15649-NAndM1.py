a9f2226b0caf86bc64dde4d2a0e0e24de4f36904
skyg547 <skyg547@gmail.com>
2021-12-29 21:34:50 +0900

    make permutation

--- BackTracking/BOJ15649-NAndM1.py
+++ BackTracking/BOJ15649-NAndM1.py
@@ -8,6 +8,29 @@ def solution(n, m):
     return answer
 
 
+# make permutation
+def permute(arr):
+    result = [arr[:]]
+    print(result)
+    c = [0] * len(arr)
+    i = 0
+    while i < len(arr):
+        print(i)
+        if c[i] < i:
+            if i % 2 == 0:
+                arr[0], arr[i] = arr[i], arr[0]
+            else:
+                arr[c[i]], arr[i] = arr[i], arr[c[i]]
+            result.append(arr[:])
+            c[i] += 1
+            i = 0
+        else :
+            c[i] = 0
+            i += 1
+    return result
+
+
 if __name__ == '__main__':
-    n, m = map(int, input().split())
-    print(solution(n, m))
+    # n, m = map(int, input().split())
+    # print(solution(n, m))
+    permute(['A', 'B' , 'C'])
