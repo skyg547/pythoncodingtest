d705dd6e84b13678542715f218cda480b0548741
skyg547 <skyg547@gmail.com>
2022-10-04 22:39:36 +0900

    BOJ25305-CutLine.py

---
+++ Sort/BOJ25305-CutLine.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/25305
+# 커트라인
+
+import sys
+
+shortcutInput = sys.stdin.readline()
+
+
+def getCutLine(candidateList, winnerCount):
+    return sorted(candidateList, reverse=True)[winnerCount-1:winnerCount:]
+
+
+if __name__ == '__main__':
+    candidate, winnerCount = map(int, shortcutInput.split())
+
+    candidateList = list(map(int, input().split()))
+    print(*getCutLine(candidateList, winnerCount))
