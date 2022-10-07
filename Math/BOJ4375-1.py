e13500fa6d426122259cd0a1ab8320843be8e7f7
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-10-07 06:30:43 +0000

    BOJ4375-1 loop on string

--- Math/BOJ4375-1.py
+++ Math/BOJ4375-1.py
@@ -3,32 +3,18 @@
 # 1로 이루어진 n의 배수 찾기
 #
 
-
 import sys
-
 shortcutInput = sys.stdin.readline
+while True:
+    try:
+        n = int(input())
 
-n = 9901
-count = 1
+        number = ''
+        while (True):
+            number += '1'
+            if (int(number) % n == 0):
+                print(len(number))
+                break
 
-while (True):
-    multipleNumber = n*count
-    count += 1
-
-    printSet = set()
-
-    strnumber = str(multipleNumber)
-    for strs in strnumber:
-        if (len(printSet) > 1):
-            break
-        printSet.add(strs)
-
-    if (len(printSet) > 1) :
-        continue
-    
-
-    if ('1' in printSet):
-        print("succes")
-        print(multipleNumber)
-        print(len(strnumber))
+    except:
         break
