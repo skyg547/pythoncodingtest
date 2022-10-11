5685076b80c076412a09a6202b20f185dca49ff1
skyg547 <skyg547@gmail.com>
2022-10-11 23:38:40 +0900

    BOJ2609-GCDAndLCM.py make Function

--- Math/BOJ2609-GCDAndLCM.py
+++ Math/BOJ2609-GCDAndLCM.py
@@ -1,34 +1,16 @@
 # https://www.acmicpc.net/problem/2609
 # 최대공약수와 최소공배수
-import sys
 import math
-from builtins import print
+import sys
 
 shortcutInput = sys.stdin.readline
 
 
-def isPrimeNumber(number):
-    for primeNumber in range(2, number):
-        if number % primeNumber == 0:
-            return False
-    return True
-
-
-def getDivisors(number):
-    return list(filter(lambda divisor: number % divisor == 0, [divisor for divisor in range(1, number + 1)]))
-
-
-def getGCD(firstNumber, secondNumber):
-    print(list(filter(isPrimeNumber, getDivisors(firstNumber))))
-    # getDivisors(secondNumber)
-    pass
-
-
-def getLCM(firstNumber, secondNumber):
-    pass
+def getLcm(firstNumber, secondNumber):
+    return (firstNumber * secondNumber // math.gcd(firstNumber, secondNumber))
 
 
 if __name__ == '__main__':
     firstNumber, secondNumber = map(int, shortcutInput().split())
     print(math.gcd(firstNumber, secondNumber))
-    # print(getGCD(firstNumber, secondNumber), getLCM(firstNumber, secondNumber))
+    print(getLcm(firstNumber, secondNumber))
