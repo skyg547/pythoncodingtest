edbfc39755f585b4ee70b81c4a788ac050e8e55c
skyg547 <skyg547@gmail.com>
2022-10-20 22:29:26 +0900

    BOJ9095-addOneTwoThree.py
    answer

--- BruteForce/BOJ9095-addOneTwoThree.py
+++ BruteForce/BOJ9095-addOneTwoThree.py
@@ -4,39 +4,41 @@
 import sys
 
 shortCutInput = sys.stdin.readline
+
+
+# f(1)
+# 1
+
+# f(2)
+# 1 +1
+# 2
+
+# f(3)
+# 1+1+1
+# 2+ 1
+# 1+2
+# 3
+
+# f(4)
+#  f(1) + f(2) + f(3)  1, 2, 3 이니 각 이전 갯수에 1 ,2, 3 을 더 해주면 뇐다
+
+
+def getNumberList():
+    tneoNumberList = [0] * 10
+    tneoNumberList[0] = 1
+    tneoNumberList[1] = 2
+    tneoNumberList[2] = 4
+
+    for index in range(3, len(tneoNumberList)):
+        tneoNumberList[index] = tneoNumberList[index - 1] + tneoNumberList[index - 2] + tneoNumberList[index - 3]
+
+    return tneoNumberList
+
+
 if __name__ == '__main__':
-    n = int(shortCutInput())
+    numberList = getNumberList()
 
-    numberList = [0]*10
-    numberList[0] = 1
-    numberList[1] = 2
-    numberList[2] = 3
+    iterableCount = int(shortCutInput())
 
-    for i in range (3, len(numberList)):
-        numberList[i] = numberList[i-1]*2 +1
-
-    print(numberList)
-
-    #
-    # 1 1 *2 +1
-    #
-    # 1+1 2
-    # 2
-    #
-    # 1+1+1 3 *2 +1
-    # 1+2 2
-    # 2+1
-    # 3
-    #
-    # 1+1+1+1    7
-    # 1+1+2
-    # 1+2+1
-    # 2+1+1
-    # 2+2
-    # 3+1 3
-    # 1+3 3
-    #
-    # 1+4 14
-    # 4+1 14 +1
-    #자기 자신 +1
-    # 1, 1+1, 1+1+1, :
+    for _ in range(iterableCount):
+        print(numberList[int(shortCutInput()) - 1])
