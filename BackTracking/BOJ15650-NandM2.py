7ce793f3d3db251d02433934a3111b460ce8485f
skyg547 <skyg547@gmail.com>
2022-10-22 00:28:10 +0900

    BOJ15650-NandM2.py

--- BackTracking/BOJ15650-NandM2.py
+++ BackTracking/BOJ15650-NandM2.py
@@ -1,13 +1,14 @@
 # https://www.acmicpc.net/problem/15650
 
 from itertools import permutations
+from itertools import combinations
 
 
 def solution(n, m):
-    return (list(permutations(range(1, 1 + n), m)))
+    return (list(combinations(range(1, 1 + n), m)))
 
 
 if __name__ == '__main__':
-    n , m = map(int , input().split())
-    for element in solution(n,m):
+    n, m = map(int, input().split())
+    for element in solution(n, m):
         print(*element)
