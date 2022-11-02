8f8d8a1afb622bf3ff7b2a89ac7a058d5ec679dc
skyg547 <skyg547@gmail.com>
2022-11-02 23:45:20 +0900

    BOJ10828-Stack.py

--- QANDS/BOJ10828-Stack.py
+++ QANDS/BOJ10828-Stack.py
@@ -1,26 +1,34 @@
-#push X: 정수 X를 스택에 넣는 연산이다.pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.size: 스택에 들어있는 정수의 개수를 출력한다.empty: 스택이 비어있으면 1, 아니면 0을 출력한다.top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다
+# https://www.acmicpc.net/problem/10828
+# 스택
+# push X: 정수 X를 스택에 넣는 연산이다.pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.size: 스택에 들어있는 정수의 개수를 출력한다.empty: 스택이 비어있으면 1, 아니면 0을 출력한다.top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다
 
-def solution(arr): 
-  stack = []
+import sys
 
-  for command in arr: 
-    commands = command.split()
+shortcutInput = sys.stdin.readline
 
-    if commands[0] == 'push' :
-      stack.append(commands[1])
-    elif commands[0] == 'size': 
-      print(len(stack))
-    elif command[0] == 'pop' :
-      print(stack.pop())
-    elif command[0] == 'top' :
-      print(stack[-1])
-    elif command[0] == 'empty' :
-      if len(stack) == 0 :  
-        print(1)
-      else :
-        print(0)
-      
-i = int(input())
 
-arr = [input() for _ in range(i)]
+def solution(arr):
+    stacks = []
+    for command in arr:
+        commands = command.split()
+        if 'push' == commands[0]:
+            stacks.append(commands[1])
+        elif 'size' == commands[0]:
+            print(len(stacks))
+        elif commands[0] == 'pop' and len(stacks) != 0:
+            print(stacks.pop())
+        elif commands[0] == 'top' and len(stacks) != 0:
+            print(stacks[-1])
+        elif commands[0] == 'empty':
+            if len(stacks) == 0:
+                print(1)
+            else:
+                print(0)
+        else:
+            print(-1)
+
+
+i = int(shortcutInput())
+
+arr = [shortcutInput() for _ in range(i)]
 solution(arr)
