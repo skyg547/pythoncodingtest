c63ad48715f701f075822017c72bfff8b9aa1672
skyg547 <skyg547@gmail.com>
2022-11-03 23:42:56 +0900

    BOJ9012-ParenthesisString.py first try

---
+++ QANDS/BOJ9012-ParenthesisString.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/9012
+# 괄호
+
+if __name__ == '__main__':
+    count = int(input())
+    parentheises = [list(input()) for _ in range(count)]
+
+    for parentheis in parentheises:
+        parentheisStack = []
+        for strings in parentheis:
+            if len(parentheisStack) != 0 and strings != parentheisStack[-1]:
+                parentheisStack.pop()
+                continue
+            parentheisStack.append(strings)
+
+        if len(parentheisStack) == 0:
+            print('YES')
+        else:
+            print('NO')
