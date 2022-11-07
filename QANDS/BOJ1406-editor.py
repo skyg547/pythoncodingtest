108714bb71463e54aba6783a547d1f0cfc58d90b
skyg547 <skyg547@gmail.com>
2022-11-07 23:28:39 +0900

    BOJ1406-editor.py timeout

--- QANDS/BOJ1406-editor.py
+++ QANDS/BOJ1406-editor.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
     orders = [shortcutInput() for _ in range(orderCount)]
 
     # 초기화
-    firstNode = ListNode('') # 가장 첫노드
+    firstNode = ListNode('')  # 가장 첫노드
     head = firstNode
+
     for string in initStirng:
         current_node = ListNode(string)
         current_node.prev = head  # 이전노드 설정
@@ -33,29 +34,30 @@ if __name__ == '__main__':
     for order in orders:
         if order[0] == 'P':
             current_node = ListNode(order[2])
-            current_node.prev = head.prev  # 추가하려는 노드 이전 설정
-            current_node.next = head  # 추가하려는 노드 다음 설정
-            head.prev.next = current_node # 현재 노드의 다음 추가하려는 노드로 설정
-            head.prev = current_node  # 추가하려는 노드
+            current_node.prev = head  # 추가하려는 노드 이전 설정
+            current_node.next = head.next  # 추가하려는 노드 다음 설정
+            head.next = current_node  # 현재 노드의 다음 추가하려는 노드로 설정
+            if current_node.next is not None:
+                current_node.next.prev = current_node  # 추가하려는 노드
             head = current_node  # 현재 노드 변경
-        elif order[0] == 'B' and head.prev is not None:
+        elif order[0] == 'B' and head is not firstNode:
+            head.prev.next = head.next  # 헤드 이전 노드 설정
             if head.next is not None:  # 헤드 다음 노드의 이전 노드 설정
-                head.prev.next = head.next
                 head.next.prev = head.prev
-            else :
-                head.prev.next = None
-        elif order[0] == 'L' and head.prev.prev is not None:
+            head = head.prev
+        elif order[0] == 'L' and head is not firstNode:
             head = head.prev
         elif order[0] == 'D' and head.next is not None:
             head = head.next
+        anserString = ''
 
-        print(head.val)
-    anserString = ''
+# print(head.val)
+anserString = ''
 
-    # print all node
-    node = firstNode
-    while node:
-        anserString += node.val
-        node = node.next
-    # for order in orders:
-    print(anserString)
+# print all node
+node = firstNode
+while node:
+    anserString += node.val
+    node = node.next
+# for order in orders:
+print(anserString)
