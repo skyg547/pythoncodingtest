1d44de97e0ed8aa9c8b2a002db510e14265e6f9d
skyg547 <skyg547@gmail.com>
2022-11-18 23:59:18 +0900

    BOJ14501-Retire.py

--- RecursiveFunction/BOJ14501-Retire.py
+++ RecursiveFunction/BOJ14501-Retire.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
                         key=lambda x: x[0])
     dateMaxProfits = [0] * (retireDate + 3)
     for interview in interviews:
-        if interview[0] + (interview[1] - 1) > retireDate:  # 은퇴날짜보다 크면 생략
-            continue
-        dateMaxProfits[interview[0]] = max(interview[2] + dateMaxProfits[interview[0] + interview[1]],
-                                           dateMaxProfits[interview[0] + 1])
-
+        if interview[0] + (interview[1] - 1) > retireDate:  # 은퇴날짜보다 이전꺼
+            dateMaxProfits[interview[0]] = dateMaxProfits[interview[0] + 1]
+        else:
+            dateMaxProfits[interview[0]] = max(interview[2] + dateMaxProfits[interview[0] + interview[1]],
+                                               dateMaxProfits[interview[0] + 1])
 
 print(dateMaxProfits[1])
