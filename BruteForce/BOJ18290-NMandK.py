12217569d1e728d67b2de24aeefee330f6038741
skyg547 <skyg547@gmail.com>
2022-11-24 00:50:51 +0900

    BOJ18290-NMandK.py third try

--- BruteForce/BOJ18290-NMandK.py
+++ BruteForce/BOJ18290-NMandK.py
@@ -12,42 +12,19 @@ if __name__ == '__main__':
     for _ in range(rowNumber):
         numberMatrix += list(map(int, input().split()))
 
-    ## 모든 경우 다 구해주기
-    print(numberMatrix)
-
     maxSum = -sys.maxsize
-    numberIndex = -1
-    lastNumber = -100002
     ## 한바퀴 돌면서
     for numbers in list(combinations(numberMatrix, selectCount)):
-        if(lastNumber != ):
-            numberIndex++
-        ## 이때 두수 인접 했을 시에는 제외 한다 1. 우측 일때 +1 인덱스. 2 아래 일때 + m  , 길이가 넘어 n 을 넘으면 안된다 .
-        if (numbers[0] == 9 and numbers[1] == 8) or \
-                (numbers[0] == 9 and numbers[1] == 8):
-            continue
-        maxSum = max(maxSum, sum(numbers))
+        ## 이때 두수 인접 했을 시에는 제외 한다
+        for numberIndex in range(len(numberMatrix)):  # 인덱스를 돌면서 찾는다
+            if numbers[0] == numberMatrix[numberIndex]:  # 첫째수가 같을때
+                if numberIndex + 1 < len(numberMatrix):  # n 보다 작고
+                    if numbers[1] == numberMatrix[numberIndex + 1]:  # 우측일때
+                        continue
+                if numberIndex + colNumber < len(numberMatrix):  # n 보다 작고
+                    if numbers[1] == numberMatrix[numberIndex + colNumber]:  # n 아래일대
+                        continue
+
+                maxSum = max(maxSum, sum(numbers))
     ## 맥스 값을 찾는다
     print(maxSum)
-
-# for index, numbers in enumerate(list(combinations(numberMatrix, selectCount))):
-#     if index % (23) == 0:
-#         print()
-#     print(numbers, index)
-# for rowindex, row in enumerate(numberMatrix):
-#     for colindex, col in enumerate(row):
-#         for selectrowindex, row in range(rowindex, rowNumber+1 ):
-#             for colindex, col in range(colindex, colNumber+1 ):
-#                 for direction in directions:
-#
-#                 print(rowindex, colindex)
-#                 print(row,col)
-# 여태까지 인덱스 제게
-
-# for indexs in range(rowNumber * colNumber):
-#     for doubleindexs in range((rowNumber * colNumber) - indexs):
-#         print(indexs + doubleindexs)
-#
-# for indexs in range(rowNumber * colNumber):
-#     for doubleindexs in range((rowNumber * colNumber) - indexs):
-#         print(indexs + doubleindexs)
