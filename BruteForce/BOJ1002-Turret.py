e794e3c05e984b99b9f249f6a83e9afda8635b04
skyg547 <skyg547@gmail.com>
2023-05-30 16:37:42 +0900

    코드 및 경로 정리

--- BruteForce/BOJ1002-Turret.py
+++ BruteForce/BOJ1002-Turret.py
@@ -1,6 +1,27 @@
 # # 터렛
 # # https://www.acmicpc.net/problem/1002
 # import math
+import math
+
+if __name__ == '__main__':
+    n = int(input())
+    inputCase = [list(map(int, input().split())) for _ in range(n)]
+
+    for location in inputCase:
+        distation = math.dist((location[0], location[1]), (location[3], location[4]))
+        if distation == 0 and location[2] - location[5] == 0: # 일치할때
+            print(-1)
+        elif abs(location[2] - location[5]) == distation or location[2] + location[5] == distation:  # 내접 외접
+            print(1)
+        elif location[2] + location[5] > distation > abs(location[2] - location[5]):  # 사이 낀거
+            print(2)
+        else:
+            print(0)  # 접촉을 안할대
+
+
+# # 터렛
+# # https://www.acmicpc.net/problem/1002
+# import math
 #
 #
 # def solution(alllists):
@@ -52,12 +73,12 @@
 #     # print(lists)
 
 
-n = int(input())
-
-for i in range(n):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    r = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
-    R = [r1, r2, r]
-    m = max(R); R.remove(m)
-    print(-1 if (r == 0 and r1 == r2) else 1 if (r == r1 + r2 or m == sum(R)) else 0 if (m> sum(R)) else 2)
+# n = int(input())
+#
+# for i in range(n):
+#     x1, y1, r1, x2, y2, r2 = map(int, input().split())
+#     r = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+#     R = [r1, r2, r]
+#     m = max(R); R.remove(m)
+#     print(-1 if (r == 0 and r1 == r2) else 1 if (r == r1 + r2 or m == sum(R)) else 0 if (m> sum(R)) else 2)
 
