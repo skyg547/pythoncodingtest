8db573bd765df3c531777e468292168d469d7acb
SungHo Ha <skyg547@daiso.co.kr>
2024-01-12 17:37:55 +0900

    Programmers_MultiplyString.py - Other Answer

--- operate/Programmers_MultiplyString.py
+++ operate/Programmers_MultiplyString.py
@@ -1,10 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181940?language=python3
 
 def solution(my_string, k):
-    answer = ''
-    for _ in range(k):
-        answer += my_string
-    return answer
+    return my_string * k
 
 
 if __name__ == '__main__':
