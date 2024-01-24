1e5bf3b6806cfa446814de842d73a8606c2425f0
SungHo Ha <skyg547@daiso.co.kr>
2024-01-24 11:13:05 +0900

    Programmers_GetNString.py - My Answer

---
+++ operate/Programmers_GetNString.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181907
+
+def solution(my_string, n):
+    return my_string[:n:]
+
+if __name__ == '__main__':
+    print(solution('He110W0r1d',5))
