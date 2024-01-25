ca55874d0b89b2841c20903957eb73f25469a57d
SungHo Ha <skyg547@daiso.co.kr>
2024-01-25 16:33:31 +0900

    Programmers_FirstNegative.py - Other Answer
    index method is one more loop so to use enumerate object

--- operate/Programmers_FirstNegative.py
+++ operate/Programmers_FirstNegative.py
@@ -1,12 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181896
 
+# index가 O(N)이므로, 최악의 경우 맨마지막에 음수가 나오면 loop 2바퀴를 돕니다. enumerate(num_list): 이용하면 좋을 듯
 def solution(num_list):
-    for number in num_list:
+    for index, number in enumerate(num_list):
         if number < 0:
-            return num_list.index(number)
-
+            return index
     return -1
 
 
 if __name__ == '__main__':
-    print(solution([13, 22, 53, 24, 15, 6]))
+    print(solution([12, 4, 15, 46, 38, -2, 15]))
